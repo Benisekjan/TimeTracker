e81e4bc2fa0aaa51d64b81ac82e252310d34334c
Benisekjan <janik.benisek@seznam.cz>
2024-09-25 15:22:32 +0200

    AktivitaOken

--- gui/menu.py
+++ gui/menu.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QMainWindow, QAction, QLabel, QVBoxLayout, QWidget, QGroupBox, QApplication
 from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import QDateTime, QTimer, QObject, pyqtSignal, QThread, pyqtSlot
+from PyQt5.QtCore import QDateTime
 from utils.activity_tracker import ActivityTracker
 
 class Menu(QMainWindow):
@@ -8,98 +8,63 @@ class Menu(QMainWindow):
         super().__init__()
         self.initUI()
 
-        # Seznamy pro ukládání aktivních a neaktivních období
         self.active_periods = []
-        self.inactive_periods = []
-        self.current_activity_start = None  # Ukládá čas začátku aktuální aktivity
-        self.current_inactivity_start = None  # Ukládá čas začátku aktuální neaktivity
+        self.current_window_start = None
+        self.current_window = None
 
-        # Vytvoření instance třídy ActivityTracker, která sleduje aktivitu
         self.activity_tracker = ActivityTracker()
-        # Připojení signálů ze sledovače aktivit k funkcím pro zpracování změn
-        self.activity_tracker.activityChanged.connect(self.handle_activity_change)
         self.activity_tracker.windowChanged.connect(self.handle_window_change)
 
     def initUI(self):
-        # Vytvoření akce pro ukončení aplikace
         exitAct = QAction(QIcon('exit.png'), '&Quit', self)
         exitAct.setShortcut('Ctrl+Q')
         exitAct.setStatusTip('Exit application')
         exitAct.triggered.connect(QApplication.instance().quit)
 
-        # Vytvoření menu
         menubar = self.menuBar()
         menubar.setNativeMenuBar(False)
 
-        # Přidání položek do menu
         fileMenu = menubar.addMenu('&File')
         fileMenu.addAction(exitAct)
 
-        editMenu = menubar.addMenu('&Edit')
-        editMenu.addAction('New')
-
-        # Nastavení okna aplikace
         self.setGeometry(100, 100, 800, 600)
         self.setWindowTitle('Simple menu')
 
-        # Vytvoření widgetu pro zobrazení aktivity
         self.create_activity_widget()
 
     def create_activity_widget(self):
-        # Widget, který zobrazuje aktivitu
         activity_widget = QWidget(self)
         self.setCentralWidget(activity_widget)
 
-        # Layout pro zobrazení obsahu
         vbox = QVBoxLayout(activity_widget)
         groupbox = QGroupBox("Activity Tracker", activity_widget)
         vbox.addWidget(groupbox)
 
-        # Štítek pro zobrazení informací o aktivitě
-        self.activity_label = QLabel("Tracking activity here...", groupbox)
+        self.activity_label = QLabel("Tracking window changes...", groupbox)
         groupbox_layout = QVBoxLayout(groupbox)
         groupbox_layout.addWidget(self.activity_label)
 
-    def handle_activity_change(self, active):
-        # Zpracování změny aktivity (aktivní/neaktivní)
-        current_time = QDateTime.currentDateTime()  # Získá aktuální čas
-        if active:
-            # Pokud uživatel začne být aktivní
-            if self.current_inactivity_start is not None:
-                # Pokud bylo předtím neaktivní období, zaznamená ho
-                if self.current_inactivity_start != current_time:
-                    self.inactive_periods.append(
-                        f"Inactive from {self.current_inactivity_start.toString('hh:mm:ss')} to {current_time.toString('hh:mm:ss')}"
-                    )
-                self.current_inactivity_start = None  # Resetuje čas začátku neaktivity
-            self.current_activity_start = current_time  # Uloží čas začátku aktivity
-        else:
-            # Pokud uživatel přestane být aktivní
-            if self.current_activity_start is not None:
-                # Pokud bylo předtím aktivní období, zaznamená ho
-                if self.current_activity_start != current_time:
-                    self.active_periods.append(
-                        f"Active from {self.current_activity_start.toString('hh:mm:ss')} to {current_time.toString('hh:mm:ss')}"
-                    )
-                self.current_activity_start = None  # Resetuje čas začátku aktivity
-                self.current_inactivity_start = current_time  # Uloží čas začátku neaktivity
-
-        self.update_display()  # Aktualizuje zobrazení aktivity
-
     def handle_window_change(self, window_name):
-        # Zpracování změny aktivního okna
-        current_time = QDateTime.currentDateTime()  # Získá aktuální čas
-        # Zaznamená změnu okna do seznamu aktivních období
+        current_time = QDateTime.currentDateTime()
+
+        # Zaznamenat konec předchozího okna, pokud existuje
+        if self.current_window is not None:
+            self.active_periods.append(
+                f"Window active {self.current_window} from {self.current_window_start.toString('hh:mm:ss')} to {current_time.toString('hh:mm:ss')}"
+            )
+
+        # Aktualizovat aktuální okno
+        self.current_window = window_name
+        self.current_window_start = current_time
+
+        # Zaznamenat změnu okna
         self.active_periods.append(
             f"Window changed to {window_name} at {current_time.toString('hh:mm:ss')}"
         )
-        self.update_display()  # Aktualizuje zobrazení aktivity
+
+        self.update_display()
 
     def update_display(self):
-        # Zobrazení seznamu aktivních a neaktivních období v rozhraní
-        active_periods_str = "\n".join(self.active_periods)  # Převede seznam aktivních období na text
-        inactive_periods_str = "\n".join(self.inactive_periods)  # Převede seznam neaktivních období na text
-
-        # Kombinuje oba seznamy do textu, který se zobrazí na štítku
-        activity_text = f"Active periods:\n{active_periods_str}\n\nInactive periods:\n{inactive_periods_str}"
-        self.activity_label.setText(activity_text)  # Zobrazí text na štítku
+        active_periods_str = "\n".join(self.active_periods)
+        activity_text = f"Window periods:\n{active_periods_str}"
+        self.activity_label.setText(activity_text)
--- utils/activity_tracker.py
+++ utils/activity_tracker.py
@@ -1,17 +1,13 @@
-from PyQt5.QtCore import QObject, pyqtSignal, QTimer
-from pynput import mouse, keyboard
+from PyQt5.QtCore import QObject, pyqtSignal
 from Foundation import NSWorkspace
-from AppKit import NSWorkspaceDidActivateApplicationNotification, NSWorkspaceDidDeactivateApplicationNotification
+from AppKit import NSWorkspaceDidActivateApplicationNotification
 
 class ActivityTracker(QObject):
-    # Signál emitující změnu aktivity (aktivní/neaktivní)
-    activityChanged = pyqtSignal(bool)
     # Signál emitující změnu okna (jméno aktuálního okna)
     windowChanged = pyqtSignal(str)
 
     def __init__(self):
         super().__init__()
-        self.active = False  # Ukládá stav aktivity uživatele (True = aktivní, False = neaktivní)
         self.current_window = None  # Ukládá jméno aktuálního okna
 
         # Nastavení sledování změn aktivního okna pomocí NSWorkspace
@@ -23,58 +19,8 @@ class ActivityTracker(QObject):
             NSWorkspaceDidActivateApplicationNotification,  # Notifikace pro aktivaci okna
             None
         )
-        self.notification_center.addObserver_selector_name_object_(
-            self,
-            "applicationDeactivated:",  # Metoda, která bude volána při deaktivaci okna
-            NSWorkspaceDidDeactivateApplicationNotification,  # Notifikace pro deaktivaci okna
-            None
-        )
-
-        # Nastavení posluchače pro myš a klávesnici
-        # Myš sleduje pohyb a kliknutí
-        self.mouse_listener = mouse.Listener(on_move=self.on_activity, on_click=self.on_activity)
-        # Klávesnice sleduje stisk kláves
-        self.keyboard_listener = keyboard.Listener(on_press=self.on_activity)
-
-        # Nastavení časovače pro sledování neaktivity
-        self.inactivity_timer = QTimer()
-        self.inactivity_timer.setInterval(3000)  # Interval pro neaktivitu je 3 sekundy
-        self.inactivity_timer.timeout.connect(self.check_inactivity)  # Po 3 sekundách zkontroluje neaktivitu
-
-    def start(self):
-        # Spustí posluchače myši a klávesnice a časovač neaktivity
-        self.mouse_listener.start()
-        self.keyboard_listener.start()
-        self.inactivity_timer.start()
-
-    def stop(self):
-        # Zastaví posluchače myši a klávesnice a časovač neaktivity
-        self.mouse_listener.stop()
-        self.keyboard_listener.stop()
-        self.inactivity_timer.stop()
-
-    def on_activity(self, *args):
-        # Když je detekována aktivita (pohyb myši, stisk klávesy), aktualizuje stav
-        if not self.active:
-            self.active = True  # Nastaví uživatele jako aktivního
-            self.activityChanged.emit(True)  # Vyvolá signál, že uživatel je aktivní
-        self.inactivity_timer.start()  # Restartuje časovač neaktivity
-
-    def check_inactivity(self):
-        # Tato metoda je volána po uplynutí 3 sekund bez aktivity
-        if self.active:
-            self.active = False  # Nastaví uživatele jako neaktivního
-            self.activityChanged.emit(False)  # Vyvolá signál, že uživatel je neaktivní
-            self.inactivity_timer.stop()  # Zastaví časovač neaktivity
 
     def applicationActivated_(self, notification):
-        # Tato metoda se spustí, když je okno aktivováno
-        self.on_activity()  # Po aktivaci okna se zaznamená aktivita
-
-    def applicationDeactivated_(self, notification):
-        # Tato metoda se spustí, když je okno deaktivováno (přepnutí na jiné okno)
-        self.check_inactivity()  # Zaznamená neaktivitu při přepnutí okna
-
         # Získání jména aktuálního aktivního okna
         active_app = self.workspace.frontmostApplication()
         window_name = active_app.localizedName() if active_app else "Unknown"  # Pokud není okno rozpoznáno, nastaví "Unknown"
